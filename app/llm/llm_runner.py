287a58843fde98c39da99b2141498eb313a10deb
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2024-09-05 18:58:50 +0530

    updated path for job manifest files

--- app/llm/llm_runner.py
+++ app/llm/llm_runner.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 import time
+import traceback
 import uuid
 
 from kubernetes import client, config
@@ -18,11 +19,12 @@ queue_url = AWS.SQS.COMPLETED_TEXTRACT_QUEUE
 # Get namespace from environment variable
 NAMESPACE = os.getenv('ENVIRONMENT')
 
+
 async def create_job(message_body, logger):
     batch_v1 = client.BatchV1Api()
 
     # TODO: Update job manifest file as per the requirement
-    job_manifest = json.load(open('llm/job_manifest.json', 'r'))
+    job_manifest = json.load(open('app/llm/job_manifest.json', 'r'))
 
     job_name = f"llm-job-{uuid.uuid1()}"
     job_manifest['metadata']['name'] = job_name
@@ -36,27 +38,31 @@ async def create_job(message_body, logger):
 
 
 async def runner():
-    sqs_helper = SQSHelper()
     logger = get_cloudwatch_logger(log_stream_name=AWS.CloudWatch.LLM_RUNNER_STREAM)
+    try:
+        sqs_helper = SQSHelper()
 
-    if not NAMESPACE:
-        logger.info('Configuration incomplete. Please configure ENVIRONMENT variable.')
-        exit(0)
+        if not NAMESPACE:
+            logger.info('Configuration incomplete. Please configure ENVIRONMENT variable.')
+            exit(0)
 
-    logger.info(f'Reading messages from queue: {queue_url.split("/")[-1]}')
-    while True:
-        message_body, receipt_handle = await sqs_helper.consume_message(queue_url)
-        if not (message_body and receipt_handle):
-            time.sleep(10)
-            continue
+        logger.info(f'Reading messages from queue: {queue_url.split("/")[-1]}')
+        while True:
+            message_body, receipt_handle = await sqs_helper.consume_message(queue_url)
+            if not (message_body and receipt_handle):
+                time.sleep(10)
+                continue
 
-        project_id, document_name = await get_project_id_and_document(message_body['DocumentLocation']['S3ObjectName'])
-        logger = get_cloudwatch_logger(project_id=project_id, document_name=document_name,
-                                       log_stream_name=AWS.CloudWatch.LLM_RUNNER_STREAM)
-        logger.info(f'Message received from queue: {queue_url.split("/")[-1]}')
-        await create_job(message_body, logger)
-        await sqs_helper.delete_message(queue_url, receipt_handle)
-        sys.stdout.flush()
+            project_id, document_name = await get_project_id_and_document(
+                message_body['DocumentLocation']['S3ObjectName'])
+            logger = get_cloudwatch_logger(project_id=project_id, document_name=document_name,
+                                           log_stream_name=AWS.CloudWatch.LLM_RUNNER_STREAM)
+            logger.info(f'Message received from queue: {queue_url.split("/")[-1]}')
+            await create_job(message_body, logger)
+            await sqs_helper.delete_message(queue_url, receipt_handle)
+            sys.stdout.flush()
+    except Exception as e:
+        logger.error('%s -> %s' % (e, traceback.format_exc()))
 
 
 if __name__ == '__main__':
--- app/textract/textract_runner.py
+++ app/textract/textract_runner.py
@@ -4,6 +4,7 @@ import os.path
 import sys
 import time
 import uuid
+import traceback
 
 from kubernetes import client, config
 
@@ -21,7 +22,7 @@ async def create_job(message_body, logger):
     batch_v1 = client.BatchV1Api()
 
     # TODO: Update job manifest file as per the requirement
-    job_manifest = json.load(open('textract/job_manifest.json', 'r'))
+    job_manifest = json.load(open('app/textract/job_manifest.json', 'r'))
 
     job_name = f"textract-job-{uuid.uuid1()}"
     job_manifest['metadata']['name'] = job_name
@@ -35,28 +36,30 @@ async def create_job(message_body, logger):
 
 
 async def runner():
-    sqs_helper = SQSHelper()
     logger = get_cloudwatch_logger(log_stream_name=AWS.CloudWatch.TEXTRACT_RUNNER_STREAM)
+    try:
+        sqs_helper = SQSHelper()
 
-    if not NAMESPACE:
-        logger.info('Configuration incomplete. Please configure ENVIRONMENT variable.')
-        exit(0)
+        if not NAMESPACE:
+            logger.info('Configuration incomplete. Please configure ENVIRONMENT variable.')
+            exit(0)
 
-    logger.info(f'Reading messages from queue: {queue_url.split("/")[-1]}')
-    while True:
-        message_body, receipt_handle = await sqs_helper.consume_message(queue_url)
-        if not (message_body and receipt_handle):
-            time.sleep(10)
-            continue
+        logger.info(f'Reading messages from queue: {queue_url.split("/")[-1]}')
+        while True:
+            message_body, receipt_handle = await sqs_helper.consume_message(queue_url)
+            if not (message_body and receipt_handle):
+                time.sleep(10)
+                continue
 
-        logger = get_cloudwatch_logger(project_id=message_body['document_path'].split('/')[2],
-                                       document_name=os.path.basename(message_body['document_path']),
-                                       log_stream_name=AWS.CloudWatch.TEXTRACT_RUNNER_STREAM)
-        logger.info(f'Message received from queue: {queue_url.split("/")[-1]}')
-        await create_job(message_body, logger)
-        await sqs_helper.delete_message(queue_url, receipt_handle)
-        sys.stdout.flush()
-
+            logger = get_cloudwatch_logger(project_id=message_body['document_path'].split('/')[2],
+                                           document_name=os.path.basename(message_body['document_path']),
+                                           log_stream_name=AWS.CloudWatch.TEXTRACT_RUNNER_STREAM)
+            logger.info(f'Message received from queue: {queue_url.split("/")[-1]}')
+            await create_job(message_body, logger)
+            await sqs_helper.delete_message(queue_url, receipt_handle)
+            sys.stdout.flush()
+    except Exception as e:
+        logger.error('%s -> %s' % (e, traceback.format_exc()))
 
 if __name__ == '__main__':
     asyncio.run(runner())
